10be5fe654a29dbf6886053267d4c4bd15a602e3
Jackson <jjlucas04biz@gmail.com>
2024-11-11 00:41:50 +1030

    Updates
    Still needs work

--- Assets/Editor/LootTableEditor.cs
+++ Assets/Editor/LootTableEditor.cs
@@ -1,3 +1,5 @@
+using System.Linq;
+using Unity.Mathematics;
 using UnityEditor;
 using UnityEditorInternal;
 using UnityEngine;
@@ -7,55 +9,83 @@ using UnityEngine.UIElements;
 public class LootTableEditor : Editor
 {
     ReorderableList list;
+    LootTable m_tbl;
     float[] range = new float[] {0, 1};
-    bool guarentee = false;
-    void OnEnable(){
-        list = new(serializedObject, serializedObject.FindProperty("m_entries"), true, true, true, true);
+    float[] dirtyRange = new float[] {0, 1};
+    float[] weightCache = new float[0];
+    //bool guarentee = false;
+    public void OnEnable(){
+        //get the table object can cal all the chances
+        m_tbl = (LootTable)target;
+        if(m_tbl.GetEntries.Count() > 0){
+            dirtyRange = new float[] {0, m_tbl.TableHigh()}; 
+            range = new float[] {dirtyRange[0], dirtyRange[1]};
+            m_tbl.CalChances(range[1], range[0]);
+            var ent = m_tbl.GetEntries;
+            weightCache = new float[ent.Count()];
+            for(int i = 0; i < ent.Count(); i++){
+                weightCache[i] = ent[i].Weight;
+            }
+        } 
+        DrawTable();
+    }
+    public override void OnInspectorGUI(){
+        //update the object that's selected
+        serializedObject.Update();
 
+        //draw the list (fr this time)
+        list.DoLayoutList();
+
+        //EDITOR & TEST STUFF
+        bool doTest = false;
+        if(m_tbl.GetEntries.Count() > 1) doTest = GUILayout.Button("Test Table");
+
+        dirtyRange[0] = EditorGUILayout.FloatField(dirtyRange[0]);
+        dirtyRange[1] = EditorGUILayout.FloatField(dirtyRange[1]);
+        
+        if(dirtyRange[0] != range[0] || dirtyRange[1] != range[1]){
+            range = dirtyRange;
+            ReloadTable();
+        }
+        for(int i = 0; i < weightCache.Length; i++){
+            if(weightCache[i] != m_tbl.GetEntries[i].Weight){
+                weightCache[i] = m_tbl.GetEntries[i].Weight;
+                ReloadTable();
+            }
+        }
+
+        if(doTest){
+            for(int i = 0; i < 100; i++){
+                Object newObject = m_tbl.GetRandomItem();
+                Debug.Log((newObject != null) ? $"Returned item {newObject.name}" : "Failed to get item (I did you set the range too low?)");
+            }
+        }
+        serializedObject.ApplyModifiedProperties();
+    }
+    private void ReloadTable(){
+        m_tbl.CalChances(range[1], range[0]);
+        DrawTable();
+    }
+    private void DrawTable(){
+        //draw the list
+        list = new(serializedObject, serializedObject.FindProperty("m_entries"), true, true, true, true);
+        //override the list drawing funcs
         list.drawElementCallback = (Rect rect, int index, bool isActive, bool isFocused) => {
             var element = list.serializedProperty.GetArrayElementAtIndex(index);
             rect.y += 2;
-            EditorGUI.PropertyField(new Rect(rect.x, rect.y, rect.width - 40, EditorGUIUtility.singleLineHeight), element.FindPropertyRelative("Item"), GUIContent.none);
+            EditorGUI.PropertyField(new Rect(rect.x, rect.y, rect.width - 100, EditorGUIUtility.singleLineHeight), element.FindPropertyRelative("Item"), GUIContent.none);
 
-            EditorGUI.PropertyField(new Rect(rect.x + rect.width - 30, rect.y, 30, EditorGUIUtility.singleLineHeight), element.FindPropertyRelative("Weight"), GUIContent.none);
+            EditorGUI.PropertyField(new Rect(rect.x + rect.width - 90, rect.y, 30, EditorGUIUtility.singleLineHeight), element.FindPropertyRelative("Weight"), GUIContent.none);
+
+            EditorGUI.BeginDisabledGroup(true);
+            EditorGUI.LabelField(new Rect(rect.x + rect.width - 50, rect.y, 50, EditorGUIUtility.singleLineHeight), m_tbl.GetEntries[index].DropChance.ToString());            
+            EditorGUI.EndDisabledGroup();
+
+            
         };
-
+        //can the lable
         list.drawHeaderCallback = (Rect rect) => {
-            EditorGUI.LabelField(rect, "Table");
+            EditorGUI.LabelField(rect, m_tbl.name);
         };
-    }
-    public override void OnInspectorGUI()
-    {
-        LootTable tbl = (LootTable)target;
-
-        serializedObject.Update();
-        if(GUILayout.Button("Get Chances")){
-            string pnt = "";
-            //sum the weights
-            foreach(var entry in tbl.GetEntries){
-                pnt += $"{entry.Item.name} chance is {entry.FindChance(tbl.CalTableWeight) * 100}%\n";
-            }
-            Debug.Log(pnt);
-        }
-        list.DoLayoutList();
-
-        bool doTest = false;
-        GUILayout.BeginHorizontal();
-        doTest = GUILayout.Button("Test Table");
-        range[0] = EditorGUILayout.FloatField(range[0]);
-        range[1] = EditorGUILayout.FloatField(range[1]);
-        if(range[0] < 0) range[0] = 0;
-        else if(range[0] > range[1]) range[1] = range[0];
-        GUILayout.EndHorizontal();
-        guarentee = GUILayout.Toggle(guarentee, new GUIContent("Should Guarentee"));
-        if(doTest){
-            float dud = tbl.CalTableWeight;
-            for(int i = 0; i < 100; i++){
-            Object newObject = tbl.GetRandomItem(range[0], range[1], guarentee);
-            Debug.Log((newObject != null) ? $"Returned item {newObject.name}" : "Failed to get item (I did you set the range too low?)");
-            }
-        }
-
-        serializedObject.ApplyModifiedProperties();
     }
 }
--- Assets/Scripts/LootTable.cs
+++ Assets/Scripts/LootTable.cs
@@ -1,107 +1,103 @@
-using System.Collections.Generic;
-using UnityEditor.Rendering;
+//using System;
 using UnityEngine;
 
 [CreateAssetMenu(fileName = "New Loot Table", menuName = "Loot System/Loot Table")]
 public class LootTable : ScriptableObject
 {
     [System.Serializable]
-    public struct TableEntry{
+    public class TableEntry{
         public Object Item;
         [Min(0)]
         public float Weight;
-        //private float chance;
-        void Set(Object newItem, float newWeight){
-            Item = newItem;
-            Weight = newWeight;
+        public float EffectiveWeight(float min, float max){
+            return DirtyEffectiveWeight = Weight >= min && Weight <= max ? Weight : 0;
         }
-        public float Chance {get; private set;}
-        public float FindChance(float tableWeight){
-            return Chance = Weight / tableWeight;
+        public float DirtyEffectiveWeight {get; private set;}
+
+        public float Chance(float tableWeight, float prevChance = 0){
+            DropChance = DirtyEffectiveWeight / tableWeight;
+            DirtyChance = DropChance + prevChance;
+            return DirtyChance;
         }
+        public float DirtyChance {get; private set;}
+        public float DropChance {get; private set;}
     }
 
     [SerializeField] private TableEntry[] m_entries = new TableEntry[0];
     public TableEntry[] GetEntries => m_entries;
-    public float GetTableWeight {get; private set;}
-    public float CalTableWeight { get{
-        GetTableWeight = 0; 
+    public float TableWeight() {
+        DirtyTableWeight = 0; 
         foreach(var entry in m_entries){
-            GetTableWeight += entry.Weight;
+            DirtyTableWeight += entry.DirtyEffectiveWeight;
         }
-        return GetTableWeight;
-    }}
-    public float[] GetTableLowHigh {get; private set;} = new float[2];
-    public float[] FindTableLowHigh{ get{
-        GetTableLowHigh = new float[] {1, 0};
+        return DirtyTableWeight;
+    }
+    public float[] DirtyLowHigh {get; private set; } = new float[] {0, 1};
+    public float DirtyTableWeight {get; private set;}
+    public float TableLow() {
+        if(DirtyLowHigh.Length == 0)
+            DirtyLowHigh = new float[2];
+        DirtyLowHigh[0] = m_entries[0].Weight;
         foreach(var entry in m_entries){
-            entry.FindChance(GetTableWeight);
-            if(entry.Chance < GetTableLowHigh[0]){
-                GetTableLowHigh[0] = entry.Chance;
-            }
-            if(entry.Chance > GetTableLowHigh[1]){
-                GetTableLowHigh[1] = entry.Chance;
+            if(entry.Weight < DirtyLowHigh[0]){
+                DirtyLowHigh[0] = entry.Weight;
             }
         }
-        return GetTableLowHigh;
-    }}
-    public Object GetRandomItem(float min = 0, float max = 1, bool guaranteeItem = false)
+        return DirtyLowHigh[0];
+    }
+    public float TableHigh() {
+        if(DirtyLowHigh.Length == 0)
+            DirtyLowHigh = new float[2];
+        DirtyLowHigh[1] = m_entries[0].Weight;
+        foreach(var entry in m_entries){
+            if(entry.Weight > DirtyLowHigh[1]){
+                DirtyLowHigh[1] = entry.Weight;
+            }
+        }
+        return DirtyLowHigh[1];
+    }
+    public float CalSingleChance(float max, int index, float min = 0){
+        m_entries[index].EffectiveWeight(min, max);
+        return m_entries[index].Chance(TableWeight(), index > 0 ? m_entries[index - 1].DirtyChance : 0);
+    }
+    public void CalChances(float max, float min = 0){
+        foreach(var entry in m_entries){
+            entry.EffectiveWeight(min, max);
+        }
+        TableWeight();
+        for(int i = 0; i < m_entries.Length; i++){
+            m_entries[i].Chance(DirtyTableWeight, i > 0 ? m_entries[i - 1].DirtyChance : 0);
+        }
+    }
+    public Object GetRandomItem(){
+        return GetRandomItem(TableHigh());
+    }
+    public Object GetRandomItem(float max, float min = 0)//, bool guaranteeItem = false)
     {
-        //Scale the given values
-        if(max > 1){
-            min /= max;
-            max /= max;
-        }
-        //Too low check
-        if(max <= FindTableLowHigh[0]){
-            Debug.LogError("Maximum value too low");
-            return null;
-        }
+        //min max validation
+        if(min > TableHigh()) min = TableHigh();
+        if(max < TableLow()) max = TableLow();
+        if(min > max) min = max;
 
-        //Clamp the values (should only effect values when guaranteed)
-        min = Mathf.Clamp(min, guaranteeItem ? GetTableLowHigh[0] : 0, guaranteeItem ? GetTableLowHigh[1] : max);
-        max = Mathf.Clamp(max, guaranteeItem ? GetTableLowHigh[0] : min, guaranteeItem ? GetTableLowHigh[1] : 1);
+        CalChances(max);
+
+        //Scale the given values
+        float norMin = min;
+        float norMax = max;
+        if(max > 1){
+            norMin /= max;
+            norMax /= max;
+        }
 
         //Generate a random number
-        float rnd = Random.Range(min, max);
-        //Clamp it between the min and max values
-        rnd = Mathf.Clamp(rnd, min, max);
-        List<Object> pool = new List<Object>();
-        //Collect every item that fits in the given range that is greater then the random number
+        float rnd = Random.Range(norMin, norMax);
+
+        //Go though until an entry is found that is greater then the rnd
         foreach(var entry in m_entries)
         {
-            entry.FindChance(CalTableWeight);
-            if((entry.Chance <= max && entry.Chance >= rnd) || (guaranteeItem && entry.Chance >= GetTableLowHigh[0] && entry.Chance <= rnd))
-                pool.Add(entry.Item);
+            if(entry.DirtyChance >= rnd)
+                return entry.Item;
         }
-
-        //Get a random item from the pool
-        int rndItem = Random.Range(0, pool.Count);
-        return (pool.Count > 0) ? pool[rndItem] : null;
-    }
-    public Object GetRandomFromHighestBand(float max = 1)
-    {
-        max = Mathf.Clamp(max, FindTableLowHigh[0], GetTableLowHigh[1]);
-
-        List<TableEntry> pool = new();
-        //Go thought the entries
-        foreach(var entry in m_entries){
-            entry.FindChance(CalTableWeight);
-            //if an entry is within the max limit
-            if(entry.Chance <= max ){
-                //and the rarity is greater
-                if(pool.Count != 0 && entry.Weight > pool[0].Weight){
-                    //reset the item pool
-                    pool.Clear();
-                }
-                if(pool.Count == 0 || entry.Weight == pool[0].Weight){
-                    //add this entry
-                    pool.Add(entry);
-                }
-            }
-        }
-
-        int rndItem = Random.Range(0, pool.Count);
-        return pool[rndItem].Item;
+        return null;
     }
 }
